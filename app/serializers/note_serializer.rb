d78502736d72a4f48366d59b76a7bcff77ba6cf2
Javier Diaz <javierdiazt406@gmail.com>
2023-11-12 01:36:24 -0400

    Add timestamps and remove notes of subjects

--- app/serializers/note_serializer.rb
+++ app/serializers/note_serializer.rb
@@ -1,5 +1,5 @@
 class NoteSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content, :note_type
+  attributes :id, :title, :content, :note_type, :updated_at, :created_at
   has_one :subject
   has_one :user
 end
--- app/serializers/subject_serializer.rb
+++ app/serializers/subject_serializer.rb
@@ -1,4 +1,4 @@
 class SubjectSerializer < ActiveModel::Serializer
-  attributes :id, :title, :area, :professor, :lapse, :notes, :power
+  attributes :id, :title, :area, :professor, :lapse, :power
   has_one :user
 end
