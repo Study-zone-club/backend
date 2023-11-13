de622acdfaf4336db4bfeca2584e04697951635a
Javier Diaz <javierdiazt406@gmail.com>
2023-11-12 21:06:22 -0400

    Signup fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  post '/login', to: 'authentication#login'
+  get '/*a', to: 'application#not_found'
+
   resources :activities
   resources :notes
   resources :schedules
   resources :subjects
   resources :tasks
-  resources :users, except: [:create]
-  post '/signup', to: 'users#create'
-  post '/login', to: 'authentication#login'
-  get '/*a', to: 'application#not_found'
+  resources :users
 end
