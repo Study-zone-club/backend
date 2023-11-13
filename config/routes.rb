a0c6e58ab1cead3f1f5b7a73f9c8a0058e076cbc
Javier Diaz <javierdiazt406@gmail.com>
2023-11-12 21:27:58 -0400

    Fix global routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  post '/login', to: 'authentication#login'
-  post '/signup', to: 'users#signup'
-  get '/*a', to: 'application#not_found'
-
   resources :activities
   resources :notes
   resources :schedules
   resources :subjects
   resources :tasks
   resources :users, except: [:create]
+
+  post '/login', to: 'authentication#login'
+  post '/signup', to: 'users#signup'
+  get '/*a', to: 'application#not_found'
 end
